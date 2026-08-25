6b2f60d07ed418e5db6d7103ec345342738d6d8c
Gemeshp <gemeshpatle59@gmail.com>
2026-08-25 18:46:34 +0530

    Day 6 question 3

---
+++ Day-06/03.py
@@ -0,0 +1,20 @@
+# 3.	Find the sum of all odd numbers from 1 to N.
+
+def sum():
+
+    try:
+
+        n = int(input("ENTER THE NUMBER HERE.: "))
+
+        sum = 0
+
+        for i in range(1,n+1,2):
+            sum += i
+
+        print(sum)
+
+    except ValueError:
+        print("please enter the valid number.")    
+
+if __name__ == "__main__":
+    sum()
