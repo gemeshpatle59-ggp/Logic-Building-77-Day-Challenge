ce2c4733d2c63fd9f5c59ce77a3b184b717a2ebe
Gemeshp <gemeshpatle59@gmail.com>
2026-09-01 16:11:14 +0530

    Day 13 question 5

---
+++ Day-13/05.py
@@ -0,0 +1,23 @@
+# Find the second smallest digit.
+
+num = 8768743
+
+def smallest_distinct(num):
+    digit = [float("inf"), float("inf")]  # [smallest, second_smallest]
+
+    while num > 0:
+        last_digit = num % 10
+
+        if last_digit < digit[0]:
+            digit[1] = digit[0]
+            digit[0] = last_digit
+
+        elif last_digit < digit[1] and last_digit != digit[0]:
+            digit[1] = last_digit
+
+        num = num // 10
+
+    return digit[1]
+
+
+print(smallest_distinct(num))               
