96d11c677952d8e91a5767e9d1a107b9a7a2d3be
Gemeshp <gemeshpatle59@gmail.com>
2026-09-10 22:03:57 +0530

    Day 21 question 5

--- Day-21/05.py
+++ Day-21/05.py
@@ -1,1 +1,25 @@
 # Convert a binary number represented as digits into decimal and validate the input
+
+def binary_decimal():
+    try:
+        n = input("ENTER YOUR NUMBER HERE.: ")
+
+        if any(digit not in "01" for digit in n):
+            print("Invalid binary number")
+            return
+        
+        decimal = 0
+        m = len(n)
+        for i in range(m):
+            decimal += (int(n[i])) * (2 ** (m-1-i))
+            
+        
+        print(f"decimal = {decimal}")
+
+
+    except ValueError:
+        print("Invalid input! Please enter an Integer.")
+    
+
+if __name__ == "__main__":
+    binary_decimal()              
