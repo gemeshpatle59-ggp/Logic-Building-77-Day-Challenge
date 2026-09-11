3d0ca31af94ae515c6287416f7a2c4d6daf8b53f
Gemeshp <gemeshpatle59@gmail.com>
2026-09-11 22:56:48 +0530

    Day 23 question 5

---
+++ Day-23/05.py
@@ -0,0 +1,22 @@
+#  Print a hollow rectangle
+
+#  Print a hollow square.
+
+
+def hallowtriangle():
+    try:
+        n = int(input("ENTER YOUR LENGTH ONLY ODD NUMBER HERE.: "))
+
+        print("* " * n)
+
+        for i in range((n)):
+            print("* " + ("  " * ((n-2)  )) + "*")
+
+        print("* " * n)
+
+    except ValueError:
+        print("Invalid input! Please enter an Integer.")
+    
+
+if __name__ == "__main__":
+    hallowtriangle()              
