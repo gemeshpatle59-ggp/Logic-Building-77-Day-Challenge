afa74e997e262a608f8ddca2c2cc5965f28ea284
Gemeshp <gemeshpatle59@gmail.com>
2026-09-13 09:13:47 +0530

    Day 25 question 2

---
+++ Day-25/02.py
@@ -0,0 +1,18 @@
+#  Print an alphabet triangle.
+
+def alphabettriangle():
+    try:
+        n = int(input("ENTER YOUR LENGTH ONLY ODD NUMBER HERE.: "))
+        m = 0
+        for i in range(1,n+1):
+            m += i
+
+            for j in range(1,m+1):
+                print(chr(64+j) ,end=" ")
+            print()
+
+    except ValueError:
+        print("Invalid input! Please enter an Integer.")
+
+if __name__ == "__main__":
+    alphabettriangle()              
