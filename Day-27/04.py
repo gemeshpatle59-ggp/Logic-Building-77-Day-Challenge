49761892da8476ed17c502ccbe9b4d2f4d46239b
Gemeshp <gemeshpatle59@gmail.com>
2026-09-15 17:37:55 +0530

    Day 27 question 4

---
+++ Day-27/04.py
@@ -0,0 +1,14 @@
+#  Write a function to count digits.
+
+
+def count_digit(n):
+    num = n
+    count = 0
+
+    while num > 0:
+        count += 1
+        num = num // 10
+
+    return ("Total digit in number is", count)
+
+print(count_digit(18761287))
