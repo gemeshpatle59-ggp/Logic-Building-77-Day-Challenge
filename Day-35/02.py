ed407fbf305c8b6f179ddd67838b5a9adb597afc
Gemeshp <gemeshpatle59@gmail.com>
2026-09-23 19:36:47 +0530

    Day 35 question 2

---
+++ Day-35/02.py
@@ -0,0 +1,10 @@
+# Find the length of a string without using len().
+
+s = "asfff"
+
+length_of_string = 0
+
+for _ in s:
+    length_of_string += 1
+
+print(f"length_of_string_is = {length_of_string}")
