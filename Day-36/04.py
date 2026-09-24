eb34d1c2df9721161db53b486e45a749c6087841
Gemeshp <gemeshpatle59@gmail.com>
2026-09-24 12:44:20 +0530

    Day 36 question 4

---
+++ Day-36/04.py
@@ -0,0 +1,21 @@
+# Find the first non-repeating character.
+
+def find_first_non_reapeating_character(chars):
+
+    hash_map = {}
+
+    for char in chars:
+        if char in hash_map:
+            hash_map[char] += 1
+
+        else:
+            hash_map[char] = 0
+
+    for value in hash_map:
+
+        if hash_map[value] == 0:
+            return value
+
+print(find_first_non_reapeating_character("aabbayabyttsarstareeplkmqwplkmqwkkllz"))
+
+        
